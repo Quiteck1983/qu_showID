1fbb165ca5351fa9fcf8654e38f44463c218e7dc
Quiteck1983 <53975057+Quiteck1983@users.noreply.github.com>
2024-05-09 15:53:27 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+```
+ensure showid
+```
+
+#### Features
+- press L-ALT (!) to show Player-IDs in Range
+
+2024_Quiteck1983
+
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'Test-Version VORP.'
+
+author 'quiteck1983.rp'
+description 'show player-id'
+version '0.7.0'
+
+shared_script {
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+}
